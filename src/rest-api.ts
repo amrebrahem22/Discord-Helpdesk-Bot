aca61a15befb38fbbfbdfbf249c500e38a4c8541
unknown <amrebrahem226@gmail.com>
2022-03-27 03:14:05 +0200

    Rest api

---
+++ src/rest-api.ts
@@ -0,0 +1,65 @@
+import {Client, ThreadChannel} from 'discord.js';
+import express from 'express';
+
+export function createRestApi(client: Client) {
+    const app = express();
+    app.use(express.json());
+
+    app.get('/messages', async (req, res) => {
+        const { threadId } = req.query;
+
+        if(!threadId) {
+            return res.status(400).send("Missing threadId");
+        }
+
+        const thread = await client.channels.fetch(threadId as string) as ThreadChannel;
+
+        if(!thread) {
+            return res.status(400).send("Thread with this ID not found");
+        }
+
+        const messages = await thread.messages.fetch();
+        return res.status(200).send(JSON.stringify(messages || []));
+    })
+
+    app.post('/message', async (req, res) => {
+        const { threadId, text } = req.body;
+
+        if(!threadId) {
+            return res.status(400).send("Missing threadId");
+        }
+        
+        if(!text) {
+            return res.status(400).send("Missing text");
+        }
+
+        const thread = await client.channels.fetch(threadId as string) as ThreadChannel;
+
+        if(!thread) {
+            return res.status(400).send("Thread with this ID not found");
+        }
+
+        await thread.send(text)
+        return res.status(200).send("Message sent");
+    })
+    
+    app.post('/resolve', async (req, res) => {
+        const { threadId } = req.body;
+
+        if(!threadId) {
+            return res.status(400).send("Missing threadId");
+        }
+
+        const thread = await client.channels.fetch(threadId as string) as ThreadChannel;
+
+        if(!thread) {
+            return res.status(400).send("Thread with this ID not found");
+        }
+
+        await thread.send("this conversation is marked as resolved and will be archived");
+        await thread.setArchived(true);
+        return res.status(200).send("Thread resolved");
+    })
+
+    return app;
+}
